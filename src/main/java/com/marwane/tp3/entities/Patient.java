f12f8aebd50646285dd5db49a497cf3546190d4d
marwane <marwanromani2018@gmail.com>
2025-04-11 17:10:31 +0100

    add validation

--- src/main/java/com/marwane/tp3/entities/Patient.java
+++ src/main/java/com/marwane/tp3/entities/Patient.java
@@ -1,18 +1,25 @@
 package com.marwane.tp3.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.Date;
 
 @Entity
-@Data @AllArgsConstructor @NoArgsConstructor
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min = 4, max = 40)
     private String nom;
     @Temporal(TemporalType.DATE)
     private Date dateNaissance;
     private boolean malade;
+    @Min(100)
+    private int score;
 }
